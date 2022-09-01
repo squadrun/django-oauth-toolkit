14ff34cf267ba2b676d3b1292f1bc88ccb0b3014
Sandal Jain <sandal.jain@squadiq.in>
2022-09-01 13:18:48 +0530

    removing already added column from new migration

--- oauth2_provider/migrations/0002_08_updates.py
+++ oauth2_provider/migrations/0002_08_updates.py
@@ -15,12 +15,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-             model_name='Application',
-             name='skip_authorization',
-             field=models.BooleanField(default=False),
-             preserve_default=True,
-        ),
         migrations.AlterField(
             model_name='Application',
             name='user',
